d3373185150e5a6ac970ae77b977c41803516797
Mark Casias <mark.casias@lullabot.com>
2024-12-11 18:07:54 -0700

    something something wrong js

--- web/themes/custom/nmrugby/components/02-molecules/menus/main-menu/main-menu.js
+++ web/themes/custom/nmrugby/components/02-molecules/menus/main-menu/main-menu.js
@@ -1,27 +1,23 @@
-Drupal.behaviors.mainMenu = {
-  attach() {
-    const menu = document.getElementById('main-nav');
-    if (menu) {
-      const toggleExpand = document.getElementById('toggle-expand');
-      const expandMenu = menu.getElementsByClassName('expand-sub');
+const menu = document.getElementById('main-nav');
+if (menu) {
+  const toggleExpand = document.getElementById('toggle-expand');
+  const expandMenu = menu.getElementsByClassName('expand-sub');
 
-      // Mobile Menu Show/Hide.
-      toggleExpand.addEventListener('click', (e) => {
-        toggleExpand.classList.toggle('toggle-expand--open');
-        menu.classList.toggle('main-nav--open');
-        e.preventDefault();
-      });
+  // Mobile Menu Show/Hide.
+  toggleExpand.addEventListener('click', (e) => {
+    toggleExpand.classList.toggle('toggle-expand--open');
+    menu.classList.toggle('main-nav--open');
+    e.preventDefault();
+  });
 
-      // Expose mobile sub menu on click.
-      Array.from(expandMenu).forEach((item) => {
-        item.addEventListener('click', (e) => {
-          const menuItem = e.currentTarget;
-          const subMenu = menuItem.nextElementSibling;
+  // Expose mobile sub menu on click.
+  Array.from(expandMenu).forEach((item) => {
+    item.addEventListener('click', (e) => {
+      const menuItem = e.currentTarget;
+      const subMenu = menuItem.nextElementSibling;
 
-          menuItem.classList.toggle('expand-sub--open');
-          subMenu.classList.toggle('main-menu--sub-open');
-        });
-      });
-    }
-  },
-};
+      menuItem.classList.toggle('expand-sub--open');
+      subMenu.classList.toggle('main-menu--sub-open');
+    });
+  });
+}
